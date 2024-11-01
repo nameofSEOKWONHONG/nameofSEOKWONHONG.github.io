f49c13956c66cc17fe19d62d39d76c16b128747f
seokwon <h20913@gmail.com>
2024-11-01 18:07:59 +0900

    update

--- MudExample/Data/User.cs
+++ MudExample/Data/User.cs
@@ -5,19 +5,56 @@ namespace MudExample.Data;
 
 public class User
 {
-    public string Id { get; set; }
+    public int Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
-    public string RoleId { get; set; }
-    public Role Role { get; set; }
+    public int RoleId { get; set; }
+    public string RoleName { get; set; }
+
+    public User()
+    {
+        
+    }
+
+    public User(int id, string name, string email, int roleId, string roleName)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Email = email;
+        this.RoleId = roleId;
+        this.RoleName = roleName;
+    }
 }
 
 public class Role
 {
-    public string Id { get; set; }
+    public int Id { get; set; }
     public string RoleName { get; set; }
     public string RoleDescription { get; set; }
     public string[] UserRoles { get; set; }
+
+    public Role()
+    {
+        
+    }
+
+    public Role(int id, string roleName, string roleDescription, string[] userRoles)
+    {
+        this.Id = id;
+        this.RoleName = roleName;
+        this.RoleDescription = roleDescription;
+        this.UserRoles = userRoles;
+    }
+
+    public static List<Role> GetRoles()
+    {
+        return new List<Role>()
+        {
+            new Role(1, "Administrator", "Administrator", null),
+            new Role(2, "User", "User", null),
+            new Role(3, "Guest", "Guest", null),
+        };
+    }
 }
 
 public interface IUserService
