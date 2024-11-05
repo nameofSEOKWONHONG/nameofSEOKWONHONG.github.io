351e80a4d2db329ef6b8a02a4b2baa9c0ff8d706
seokwonhong <h20913@gmail.com>
2024-11-05 21:42:53 +0900

    Revert "update"
    This reverts commit c37ba9423b428290fa22decd593b5acf865b05c4.

--- MudExample/ViewModels/MenuViewModel.cs
+++ MudExample/ViewModels/MenuViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System.Net.Http.Json;
 using Blazor.SubtleCrypto;
-using Blazored.LocalStorage;
 using eXtensionSharp;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
@@ -14,21 +13,14 @@ public class MenuViewModel
     public List<Menu> Menus { get; private set; }
     public string? SelectedHref { get; set; }
 
-    private readonly NavigationManager _navigationManager;
-    private readonly ILocalStorageService _localStorage;
-
-    public MenuViewModel(IMenuService menuService, NavigationManager navigationManager, ILocalStorageService localStorageService)
+    public MenuViewModel(IMenuService menuService)
     {
         _menuService = menuService;
-        _navigationManager = navigationManager;
-        _localStorage = localStorageService;
     }
 
     public async Task InitializeAsync()
     {
         this.Menus = await _menuService.GetMenuListAsync();
-        var href = await _localStorage.GetItemAsync<string>("href");
-        this.SelectedHref = href;
     }
 
     public IEnumerable<Menu> GetAllMenus()
@@ -51,12 +43,5 @@ public class MenuViewModel
         }
 
         return menus;
-    }
-
-    public async Task NavigateTo(string href)
-    {
-        this.SelectedHref = href;
-        _navigationManager.NavigateTo(href);
-        await _localStorage.SetItemAsync(nameof(href), href);
     }
 }
