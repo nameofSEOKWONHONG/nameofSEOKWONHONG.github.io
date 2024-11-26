5bb60ab0e9c7b3479973a1f404035efdf9b29734
seokwon <h20913@gmail.com>
2024-11-26 17:44:06 +0900

    add dashboard?

--- MudExample/Pages/Home.razor.cs
+++ MudExample/Pages/Home.razor.cs
@@ -1,4 +1,8 @@
-﻿using MudBlazor;
+﻿using System.Runtime.InteropServices.JavaScript;
+using ApexCharts;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using MudExample.Data;
 
 namespace MudExample.Pages;
 
@@ -34,4 +38,75 @@ public partial class Home
         new ChartSeries() { Name = "Sweden", Data = new double[] { 8, 6, 11, 13, 4, 16, 10, 16, 18 } },
     };
     public string[] XAxisLabels = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep" };
+
+    private List<Order> orders { get; set; } = new List<Order>()
+    {
+        new Order()
+        {
+            CustomerName = "John Doe",
+            Country = "Korea",
+            OrderDate = DateTime.Now,
+            OrderType = OrderType.Mail,
+            GrossValue = new Random().Next(100000, 1000000),
+            DiscountPercentage = 20,
+        },
+        new Order()
+        {
+            CustomerName = "Jane Smith",
+            Country = "USA",
+            OrderDate = DateTime.Now.AddDays(-5),
+            OrderType = OrderType.Web,
+            GrossValue = new Random().Next(100000, 1000000),
+            DiscountPercentage = 10,
+        },
+        new Order()
+        {
+            CustomerName = "Alice Johnson",
+            Country = "Canada",
+            OrderDate = DateTime.Now.AddDays(-10),
+            OrderType = OrderType.Phone,
+            GrossValue = new Random().Next(100000, 1000000),
+            DiscountPercentage = 15,
+        },
+        new Order()
+        {
+            CustomerName = "Bob Lee",
+            Country = "Australia",
+            OrderDate = DateTime.Now.AddDays(-20),
+            OrderType = OrderType.Contract,
+            GrossValue = new Random().Next(100000, 1000000),
+            DiscountPercentage = 5,
+        }
+    };
+
+    public class Order
+    {
+        public Guid OrderId { get; set; } = Guid.NewGuid();
+        public string CustomerName { get; set; }
+        public string Country { get; set; }
+        public DateTimeOffset OrderDate { get; set; }
+        public OrderType OrderType { get; set; }
+        public decimal GrossValue { get; set; }
+        public decimal NetValue { get =>  GrossValue * (1 - (DiscountPercentage / 100)) ; }
+        public decimal DiscountPercentage { get; set; }
+    }
+
+    public enum OrderType
+    {
+        Web, Contract, Mail, Phone
+    }
+
+    private ApexChartOptions<Order> _options;
+    [Inject] LayoutState LayoutState { get; set; }
+    protected override void OnInitialized()
+    {
+        _options = new ApexChartOptions<Order>()
+        {
+            Theme = new Theme()
+            {
+                Mode = LayoutState.IsDark ? Mode.Dark : Mode.Light,
+                Palette = PaletteType.Palette1,
+            }
+        };
+    }
 }
